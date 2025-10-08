b08d07fe05924065c04e966e42aaa3e32929dfed
mijanConnect <mijanur5rahman@gmail.com>
2025-10-08 17:00:12 +0600

    Add Pigeon design updated

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    // Local run
-    host: "10.10.7.21",
-    port: 3001,
-
-    // Host run
-    // host: "50.6.200.33",
-    // port: 3001,
-  },
-});
-
 // import react from "@vitejs/plugin-react";
 // import { defineConfig } from "vite";
 
 // export default defineConfig({
 //   plugins: [react()],
 //   server: {
+//     // Local run
+//     host: "10.10.7.21",
+//     port: 3001,
+
+//     // Host run
 //     // host: "50.6.200.33",
-//     host: "ftp.thepigeonhub.com",
-//     // host: "206.162.244.155",
-//     // port: 3035,
+//     // port: 3001,
 //   },
 // });
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // host: "50.6.200.33",
+    host: "ftp.thepigeonhub.com",
+    // host: "206.162.244.155",
+    // port: 3035,
+  },
+});
