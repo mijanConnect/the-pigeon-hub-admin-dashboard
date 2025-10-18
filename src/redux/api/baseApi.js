6083f1018ef1b7ac9a97e1cd201d706e3ca71d8b
mijanConnect <mijanur5rahman@gmail.com>
2025-10-18 11:54:42 +0600

    Complete update

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.10.7.41:5001/api/v1",
-    // baseUrl: "https://ftp.thepigeonhub.com/api/v1",
+    // baseUrl: "http://10.10.7.41:5001/api/v1",
+    baseUrl: "https://ftp.thepigeonhub.com/api/v1",
 
     prepareHeaders: (headers, { endpoint }) => {
       // headers.set("ngrok-skip-browser-warning", "true");
