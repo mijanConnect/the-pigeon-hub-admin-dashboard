ca75f5cdd7d8ef58dc76368adac6dd9cd9b47697
Mijanur Rahman <mijan.connect@gmail.com>
2025-12-14 14:38:23 +0600

    Bse api fixed

--- src/components/common/imageUrl.jsx
+++ src/components/common/imageUrl.jsx
@@ -8,7 +8,7 @@ export const getImageUrl = (path) => {
     return path;
   } else {
     // const baseUrl = "http://10.10.7.41:5001";
-    const baseUrl = "https://ftp.thepigeonhub.com";
+    const baseUrl = "https://api.thepigeonhub.com";
 
     // const baseUrl = "http://206.162.244.155:5001";
     return `${baseUrl}/${path}`;
